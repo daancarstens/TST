79df75d09459c516f65a0c9c930750ab3bffb977
daancarstens <55231123+daancarstens@users.noreply.github.com>
2020-09-15 10:47:05 +0200

    Add files via upload

---
+++ ding.js
@@ -0,0 +1,15 @@
+function Student(id, klas, studentNaam) {
+  let studentNummer = id;
+  let klasNaam = klas;
+  let naamStudent = studentNaam;
+}
+
+let student1 = new Student('28462', 'MD2Aa', 'Daan');
+let student2 = new Student('29215', 'MD2Aa', 'Rick');
+let student3 = new Student('29147', 'MD2Aa', 'Milo');
+let student4 = new Student('29118', 'MD2Aa', 'Pepijn');
+let student5 = new Student('29253', 'MD2Aa', 'Teun');
+let student6 = new Student('12700', 'MD2Aa', 'Lau');
+let student7 = new Student('29743', 'MD2Aa', 'Bram');
+let student8 = new Student('28746', 'MD2Aa', 'Romer');
+let student9 = new Student('29093', 'MD2Aa', 'Max');
